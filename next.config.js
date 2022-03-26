5321a4854e783d6e6fb1cb58876fbf380928c36a
tommykw <destinys.lotus.1108mn@gmail.com>
2022-03-26 11:52:20 +0900

    Remove unnecessary settings

--- next.config.js
+++ next.config.js
@@ -1,10 +1,3 @@
 module.exports = {
   reactStrictMode: true,
-};
-
-const withTM = require("next-transpile-modules")([
-  "react-every-layout",
-  "react-every-layout",
-]);
-
-module.exports = withTM();
+}
