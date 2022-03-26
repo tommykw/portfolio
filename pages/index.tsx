d1d5e23a62ae08f927fbd9a6fcc4484dbba24123
tommykw <destinys.lotus.1108mn@gmail.com>
2022-03-26 11:49:40 +0900

    Fine tuning

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
-    <div className={styles.container}>
+    <div className={styles['container-root']}>
       <Head>
         <title>Portfolio | Kenji Tomita</title>
         <meta
@@ -16,244 +16,225 @@ export default function Home() {
       </Head>
 
       <header>
-        <Frame className={styles.header} ratio="1:3">
+        <Frame className={styles['header-frame']} ratio="1:3">
           <Box>
             <Center andText gutters="0" intrinsic={false} max="var(--measure)">
-              <Box>
-                <img
-                  className={styles.profileicon}
-                  src="/image/icon.png"
-                  width="64"
-                  height="64"
-                  alt="icon"
-                />
-              </Box>
-              <div className={styles.textprimary}>tommykw</div>
-              <div className={styles.textprimary}>(Kenji Tomita)</div>
+              <h1>Beyond Average</h1>
+              <Image src="/image/icon.png" width="64" height="64" alt="icon" />
+              <div className={styles['header-text']}>tommykw</div>
+              <div className={styles['header-text']}>(Kenji Tomita)</div>
             </Center>
           </Box>
         </Frame>
       </header>
 
-      <main className={styles.main_container}>
-        <Center andText gutters="0" intrinsic={false} max="var(--measure)">
-          <h2 id="about_me">About Me</h2>
-          <p className={styles.description}>
-            Kenji Tomita
+      <main className={styles['container-content']}>
+        <section>
+          <Center andText gutters="0" intrinsic={false} max="var(--measure)">
+            <h2 id="about_me">About Me</h2>
+            <p className={styles['about-me-description']}>
+            Hi, my name is Kenji Tomita. I am a Web Developer based in Tokyo, Japan. 
             <br />
             <br />
-            I am a Web Developer based in Tokyo, Japan.
+            I am passionate about making programming interesting and approachable for all. And besides, I believe the web should be open and safe for everyone. In my spare time, I love to sing gospel songs.            
             <br />
-            I am passionate about making programming interesting and
-            approachable for all. <br />
-            In my spare time, I love to sing gospel songs.
-          </p>
-        </Center>
-        <br /><br />
+            <br />
+            For collaboration, email me at destinys.lotus.1108mn@gmail.com.
+            </p>
+          </Center>
+        </section>
 
-        <Center andText gutters="0" intrinsic={false} max="var(--measure)">
-          <h2 id="articles">Articles</h2>
-        </Center>
-
-        <Center andText gutters="0" intrinsic={false} max="820px">
-          <Grid isWide={true} min="250px" space="1rem">
-            <div className={styles.card}>
-              <a href="https://zenn.dev/tommykw">
-                <div>
-                  <picture>
-                    <img
-                      className={styles.output_image}
-                      src="/zenn2.png"
+        <section>
+          <Center andText gutters="0" intrinsic={false} max="820px">
+            <h2 id="articles">Articles</h2>
+            <Grid isWide={true} min="250px" space="1rem">
+              <div className={styles['article-card']}>
+                <a href="https://zenn.dev/tommykw">
+                  <div>
+                    <Image
+                      width={260}
+                      height={260}
+                      src="/image/article/zenn.png"
                       alt="Zenn"
                     />
-                  </picture>
-                  <h3>Zenn</h3>
-                  <p>Post articles about the web.</p>
-                </div>
-              </a>
-            </div>
+                    <h3>Zenn</h3>
+                    <p>Post articles about the web.</p>
+                  </div>
+                </a>
+              </div>
 
-            <div className={styles.card}>
-              <a href="https://speakerdeck.com/tommykw">
-                <div>
-                  <picture>
-                    <img
-                      className={styles.output_image}
-                      src="/speackerdeck.png"
+              <div className={styles['article-card']}>
+                <a href="https://speakerdeck.com/tommykw">
+                  <div>
+                    <Image
+                      width={260}
+                      height={260}
+                      src="/image/article/speackerdeck.png"
                       alt="Speackerdeck"
                     />
-                  </picture>
-                  <h3>Speakerdeck</h3>
-                  <p>This is a presentation material about the web.</p>
-                </div>
-              </a>
-            </div>
+                    <h3>Speakerdeck</h3>
+                    <p>This is a presentation material about the web.</p>
+                  </div>
+                </a>
+              </div>
 
-            <div className={styles.card}>
-              <a href="https://dev.to/tommykw">
-                <div>
-                  <picture>
-                    <img
-                      className={styles.output_image}
-                      src="/devto.png"
+              <div className={styles['article-card']}>
+                <a href="https://dev.to/tommykw">
+                  <div>
+                    <Image
+                      width={260}
+                      height={260}
+                      src="/image/article/devto.png"
                       alt="Dev"
                     />
+                    <h3>Dev</h3>
+                    <p>Share information about the web.</p>
+                  </div>
+                </a>
+              </div>
+            </Grid>
+          </Center>
+        </section>
+
+        <section>
+          <Center andText gutters="0" intrinsic={false} max="820px">
+            <h2 id="projects">Projects</h2>
+            <Grid isWide={true} min="250px" space="1rem">
+              <Box className={styles['project-box']}>
+                <a href="https://github.com/tommykw/pull-request-reviewer-reminder-action">
+                  <picture>
+                    <img src="https://gh-card.dev/repos/tommykw/pull-request-reviewer-reminder-action.svg" />
                   </picture>
-                  <h3>Dev</h3>
-                  <p>This is a presentation material about the web.</p>
-                </div>
-              </a>
-            </div>
-          </Grid>
-        </Center>
-        <br /><br />
-
-        <Center andText gutters="0" intrinsic={false} max="30ch">
-          <h2 id="projects">Projects</h2>
-          <Grid isWide={true} min="120px" space="1rem">
-            <Box
-            borderWidth="0px" 
-              invert={false} 
-              padding="var(--s0)"
-              className={styles.output_github}
-            >
-              <a href="https://github.com/tommykw/pull-request-reviewer-reminder-action"><img src="https://gh-card.dev/repos/tommykw/pull-request-reviewer-reminder-action.svg" /></a>
-            </Box>
-            <Box
-            borderWidth="0px" 
-              invert={false} 
-              padding="var(--s0)"
-              className={styles.output_github}
-            >
-              <a href="https://github.com/tommykw/pull-request-reviewee-reminder-action"><img src="https://gh-card.dev/repos/tommykw/pull-request-reviewee-reminder-action.svg" /></a>
-            </Box>
-            <Box
-            borderWidth="0px" 
-              invert={false} 
-              padding="var(--s0)"
-              className={styles.output_github}
-            >
-              <a href="https://github.com/tommykw/pull-request-reviewee-reminder-action"><img src="https://gh-card.dev/repos/tommykw/pull-request-reviewee-reminder-action.svg" /></a>
-            </Box>
-          </Grid>
-        </Center>
-
-        <Center andText gutters="0" intrinsic={false} max="300px">
-          <h2 id="skills">Skills</h2>
-        </Center>
-        <br /><br />
-
-        <Center andText gutters="5" intrinsic={false} max="30ch">
-          <Cluster align="center" justify="center" space="5rem">
-            <div className={styles.skill_box}>
-              <Box borderWidth="0px" invert={false} padding="var(--s0)">
-                <a href="https://www.typescriptlang.org/">
-                  <img
-                    src="/image/skill/ts-logo.png"
-                    alt="ts"
-                    width={64}
-                    height={64}
-                  />
                 </a>
               </Box>
 
-              <Box borderWidth="0px" invert={false} padding="var(--s0)">
-                <a href="https://developer.mozilla.org/en-US/docs/Glossary/CSS">
-                  <img
-                    className={styles.logoimage}
-                    src="/image/skill/css-logo.png"
-                    alt="ts"
-                    width={64}
-                    height={64}
-                  />
+              <Box className={styles['project-box']}>
+                <a href="https://github.com/tommykw/pull-request-reviewee-reminder-action">
+                  <picture>
+                    <img src="https://gh-card.dev/repos/tommykw/pull-request-reviewee-reminder-action.svg" />
+                  </picture>
                 </a>
               </Box>
+            </Grid>
+          </Center>
+        </section>
 
-              <Box borderWidth="0px" invert={false} padding="var(--s0)">
-                <a href="https://reactjs.org/">
-                  <img
-                    className={styles.logoimage}
-                    src="/image/skill/react-logo.png"
-                    alt="ts"
-                    width={64}
-                    height={64}
-                  />
-                </a>
+        <section>
+          <Center andText gutters="5" intrinsic={false} max="20ch">
+            <h2 id="skills">Skills</h2>
+            <br />
+            <Cluster align="center" justify="center" space="2rem">
+              <div className={styles['skill-box']}>
+                <Box borderWidth="0px" invert={false} padding="var(--s0)">
+                  <a href="https://www.typescriptlang.org/">
+                    <Image
+                      src="/image/skill/ts-logo.png"
+                      alt="ts"
+                      width={64}
+                      height={64}
+                    />
+                  </a>
+                </Box>
+
+                <Box borderWidth="0px" invert={false} padding="var(--s0)">
+                  <a href="https://developer.mozilla.org/en-US/docs/Glossary/CSS">
+                    <Image
+                      src="/image/skill/css-logo.png"
+                      alt="ts"
+                      width={64}
+                      height={64}
+                    />
+                  </a>
+                </Box>
+
+                <Box borderWidth="0px" invert={false} padding="var(--s0)">
+                  <a href="https://reactjs.org/">
+                    <Image
+                      src="/image/skill/react-logo.png"
+                      alt="ts"
+                      width={64}
+                      height={64}
+                    />
+                  </a>
+                </Box>
+
+                <Box borderWidth="0px" invert={false} padding="var(--s0)">
+                  <a href="https://kotlinlang.org/">
+                    <Image
+                      src="/image/skill/kotlin-logo.png"
+                      alt="ts"
+                      width={64}
+                      height={64}
+                    />
+                  </a>
+                </Box>
+              </div>
+            </Cluster>
+          </Center>
+        </section>
+
+        <section>
+          <Center
+            className={styles['carrer-container']}
+            andText
+            gutters="0"
+            intrinsic={false}
+            max="50ch"
+          >
+            <h2 id="carrer">Carrer</h2>
+            <Stack recursive={false} space="1rem">
+              <Box
+                className={styles['carrer-box']}
+                borderWidth="1px #e2e8f0"
+                invert={false}
+                padding="1em"
+              >
+                <p className={styles['carrer-box-date']}>2016 - present</p>
+                <p>Connehito, Inc. / Android Enginner. Frontend Enginner</p>
               </Box>
-
-              <Box borderWidth="0px" invert={false} padding="var(--s0)">
-                <a href="https://kotlinlang.org/">
-                  <img
-                    className={styles.logoimage}
-                    src="/image/skill/kotlin-logo.png"
-                    alt="ts"
-                    width={64}
-                    height={64}
-                  />
-                </a>
+              <Box
+                className={styles['carrer-box']}
+                borderWidth="1px #e2e8f0"
+                invert={false}
+                padding="1em"
+              >
+                <p className={styles['carrer-box-date']}>2014 - 2016</p>
+                <p>Diverse, Inc. / Android Enginner</p>
               </Box>
-            </div>
-          </Cluster>
-        </Center>
-
-        <br /><br /><br /><br />
-
-        <Center andText gutters="0" intrinsic={false} max="50ch">
-          <h2 id="carrer">Carrer</h2>
-          {/* Stack */}
-          <Stack recursive={false} space="2rem">
-            <Box
-              className={styles.carrer_box}
-              borderWidth="1px #e2e8f0"
-              invert={false}
-              padding="1em"
-            >
-              <p className={styles.carrer_box_date}>2016 - present</p>
-              <p>Connehito, Inc. / Android Enginner. Frontend Enginner</p>
-            </Box>
-            <Box
-              className={styles.carrer_box}
-              borderWidth="1px #e2e8f0"
-              invert={false}
-              padding="1em"
-            >
-              <p className={styles.carrer_box_date}>2014 - 2016</p>
-              <p>Diverse, Inc. / Android Enginner</p>
-            </Box>
-            <Box
-              className={styles.carrer_box}
-              borderWidth="1px #e2e8f0"
-              invert={false}
-              padding="1em"
-            >
-              <p className={styles.carrer_box_date}>2010 - 2014</p>
-              <p>livedoor Co.,Ltd. / Web Enginner. Android Enginner</p>
-            </Box>
-            <Box
-              className={styles.carrer_box}
-              borderWidth="1px #e2e8f0"
-              invert={false}
-              padding="1em"
-            >
-              <p className={styles.carrer_box_date}>2008 - 2010</p>
-              <p>Any, Inc. / Web Enginner</p>
-            </Box>
-          </Stack>
-        </Center>
-        <br />
+              <Box
+                className={styles['carrer-box']}
+                borderWidth="1px #e2e8f0"
+                invert={false}
+                padding="1em"
+              >
+                <p className={styles['carrer-box-date']}>2010 - 2014</p>
+                <p>livedoor Co.,Ltd. / Web Enginner. Android Enginner</p>
+              </Box>
+              <Box
+                className={styles['carrer-box']}
+                borderWidth="1px #e2e8f0"
+                invert={false}
+                padding="1em"
+              >
+                <p className={styles['carrer-box-date']}>2008 - 2010</p>
+                <p>Any, Inc. / Web Enginner</p>
+              </Box>
+            </Stack>
+          </Center>
+        </section>
       </main>
 
       <footer className={styles.footer}>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          © 2022 - present Kenji Tomita | Powered by{" "}
-          <span className={styles.logo}>
+        © 2022 - present Kenji Tomita | Powered by{" "}
+        <span className={styles['footer-logo']}>
+          <a
+            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
             <Image src="/vercel.svg" alt="Vercel Logo" width={72} height={16} />
-          </span>
-        </a>
+          </a>
+        </span>
       </footer>
     </div>
   );
