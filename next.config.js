7bcaef894b5bbf8ce2ca4a72ed14d22315ccd8ea
tommykw <destinys.lotus.1108mn@gmail.com>
2022-03-26 12:37:41 +0900

    Add react-every-layout settings

--- next.config.js
+++ next.config.js
@@ -1,3 +1,9 @@
 module.exports = {
   reactStrictMode: true,
 };
+
+const withReactEveryLayout = require("next-transpile-modules")([
+  "react-every-layout",
+]);
+
+module.exports = withReactEveryLayout();
